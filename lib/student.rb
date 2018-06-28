76326dfd7383c4bf6ff18a08acc41833082f45f8
Kayla Reopelle <kreopelle@gmail.com>
2018-06-28 20:31:15 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,10 +44,14 @@ class Student
     student = Student.new(name, grade)
     student.save
     student
-  end 
+  end
 
   def self.new_from_db(row)
     student = self.new
-    
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student
+  end 
 
 end
