992a9bc9b273fa6115b46cbdf6a5ee77a854d032
Kayla Reopelle <kreopelle@gmail.com>
2018-06-28 20:11:29 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,7 +4,7 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
 
-  def inititalize(name, grade, id = nil)
+  def inititalize(name, grade, id=nil)
     @name = name
     @grade = grade
     @id = id
