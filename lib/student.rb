de603be7f921a06a78813d037c10f877ea5f4ad7
Kayla Reopelle <kreopelle@gmail.com>
2018-06-28 20:16:15 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,9 +14,9 @@ class Student
   #   CREATE TABLE IF NOT EXISTS students (
   #     id INTEGER PRIMARY KEY,
   #     name TEXT,
-  #     grade TEXT)
+  #     grade TEXT
+  #   )
   #   SQL
-  #
   #   DB[:conn].execute(sql)
   # end
 
