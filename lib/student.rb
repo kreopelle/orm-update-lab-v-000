d5ab64c7dfd1ec6f98307baa704bfb96947269e2
Kayla Reopelle <kreopelle@gmail.com>
2018-06-28 20:09:24 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,6 +11,13 @@ class Student
   end
 
   def self.create_table
-
+    sql = <<-SQL
+    CREATE TABLE students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade INTEGER
+    ) 
+    SQL
+    
 
 end
